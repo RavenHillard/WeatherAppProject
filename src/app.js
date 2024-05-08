089a92dc0382f1d05320be4fd001e736382f8064
RavenHillard <132983617+RavenHillard@users.noreply.github.com>
2024-05-07 22:05:00 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,34 +1,28 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperaure.current;
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
-  let cityElement = document.querySelector("#city");
 
-cityElement.innerHTML = response.data.city;
-descriptionElement.innerHTML = response.data.condition.description;
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
+  getForecast(response.data.city);
 }
 
 function formatDate(date){
     let minutes = date.getMinutes();
     let hours = date.getHours();
-    let day = date.getDay();
-
-    if(minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-    
-    if (hours < 10) {
-        hours = `0${hours}`;
-    }
-
     let days = [
         "Sunday",
         "Monday",
@@ -38,13 +32,13 @@ function formatDate(date){
         "Friday",
         "Saturday",
     ];
-    let formattedDay = days[day];
-    return `${formattedDay} ${hours}:${minutes}`;
-}
+    let day = days[date.getDay()];
 
-let timeElement = document.querySelector("#time");
-let time = new Date(response.data.time * 1000);
-timeElement.innerHTML = formatDate(date);
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
+}
   
   function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
@@ -57,8 +51,47 @@ timeElement.innerHTML = formatDate(date);
     let searchInput = document.querySelector("#search-input");
     searchCity(searchInput.value);
   }
-  
-let searchFormElement = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchSubmit);
 
-  searchCity("Mexico");
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+  }
+
+  function getForecast(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+  
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+  if (index < 5) {
+  forecastHtml =
+   forecastHtml +
+  `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+    <div class="weather-forecast-icon"></div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>${Math.round(day.temperature.maximum)}°</strong></div>
+      <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
+      </div>
+    </div>
+    `;
+    }
+})
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Mexico City");
