ce659a41f25171bfc886f2ead332e76a829d2f96
RavenHillard <132983617+RavenHillard@users.noreply.github.com>
2024-05-09 01:24:27 -0500

    Update app.js
    closing all divs helps ensure alignment on page

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ function formatDate(date){
 
   function searchSubmit (event) {
     event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
+    let searchInput = document.querySelector("#search-form-input");
     searchCity(searchInput.value);
   }
 
@@ -76,7 +76,6 @@ function displayForecast(response) {
   <div class="weather-forecast-day">
     <div class="weather-forecast-date">${formatDay(day.time)}</div>
       <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
-    <div class="weather-forecast-icon"></div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
         <strong>${Math.round(day.temperature.maximum)}°</strong></div>
